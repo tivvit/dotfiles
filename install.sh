0a347ca80bd66b2719f569fbf8a02f1190b7feeb
Vit Listik <tivvit@seznam.cz>
2016-12-12 17:56:20 +0100

    i3 settings

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 for i in .??*; do
-	if [ "$i" != ".git" ] 
+	if [ "$i" != ".git" ]
 	then
 		echo $i;
 		ln -s $PWD/$i $HOME/$i
 	fi;
 done
+
+# i3
+mkdir -p $HOME/.i3/
+ln -s $PWD/i3_config  $HOME/.i3/config
